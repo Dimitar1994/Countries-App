3aed41f3784ef2e815ae36738c73f0318b8e9152
Dimitar1994 <mitaka1884@gmail.com>
2023-05-29 17:01:48 +0300

    search

---
+++ main.js
@@ -0,0 +1,24 @@
+// constants
+const BASE_URL = 'https://restcountries.com/v3.1';
+
+// elements
+const countryTemplate = document.getElementById('country-template');
+const countriesList = document.getElementById('countries-list');
+const switchButtons = document.querySelector('.countries-list-switch');
+const searchForm = document.getElementById('search-form');
+const searchResultsList = document.getElementById('search-result-list');
+const searchFormInput = searchForm.querySelector('input[name="country"]');
+
+// request api
+
+// get country clone
+
+// render countries
+
+// load countries list
+
+// list switcher
+
+// load search results
+
+// search form
